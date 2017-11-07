5fb5ab8b192001a6c979db0f026554d210b46364
Sagar Ghimire <info@sagarg.com.np>
2017-11-07 11:48:23 +0545

    hackathon page updated

---
+++ hackathon/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/soulrider911/pen/dGuEn.
+
+ Fun example of a sticky header utilizing some CSS3 transitions!
---
+++ hackathon/js/index.js
@@ -0,0 +1,27 @@
+$(function(){
+  $(window).scroll(function(){
+    var winTop = $(window).scrollTop();
+    if(winTop >= 30){
+      $("body").addClass("sticky-header");
+    }else{
+      $("body").removeClass("sticky-header");
+    }//if-else
+  });//win func.
+});//ready func.
+$('#nav').onePageNav({
+	currentClass: 'current',
+	changeHash: false,
+	scrollSpeed: 750,
+	scrollThreshold: 0.5,
+	filter: '',
+	easing: 'swing',
+	begin: function() {
+		//I get fired when the animation is starting
+	},
+	end: function() {
+		//I get fired when the animation is ending
+	},
+	scrollChange: function($currentListItem) {
+		//I get fired when you enter a section and I pass the list item of the section
+	}
+});
